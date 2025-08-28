c6ca9223098a5ba27568405678f3d39dc3fbea91
Hoang-Anh Nguyen <anhnguyen@petarainsoft.com>
2025-08-28 16:20:36 +0700

    Implement auto CSS loading from source - fix installation errors

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-// Auto-import CSS
-import './myroom-system.css';
-
-// Re-export everything from the main bundle
-export * from './myroom-system.es.js';
--- dist/myroom-system.es.js
+++ dist/myroom-system.es.js
@@ -1,25 +1,23 @@
-import { y as n, O as t, N as i, Q as m, $ as r, M as R, z as M, _ as y, V as A, X as g, Y as p, Z as u, J as C, a2 as E, a5 as O, a1 as _, a0 as F, a3 as I, a4 as N } from "./index-CeftPFLf.js";
-import "react/jsx-runtime";
-import "react";
+import { y as s, O as o, N as n, Q as t, $ as i, M as m, z as R, _ as M, V as r, X as y, Y as A, Z as g, J as u, a2 as C, a5 as E, a1 as O, a0 as _, a3 as F, a4 as I } from "./index-CeftPFLf.js";
 export {
-  n as ApiService,
-  t as DEFAULT_AVATAR_CONFIG,
-  i as DEFAULT_ROOM_CONFIG,
-  m as DEFAULT_SCENE_CONFIG,
-  r as ManifestDropdown,
-  R as MyRoom,
-  M as MyRoomRepository,
-  y as MyRoomScene,
-  A as VERSION,
-  g as domainConfig,
-  p as getEmbedUrl,
-  u as initializeMyRoomConfig,
-  C as myRoomRepository,
-  E as useAvatarManagement,
-  O as useBackendData,
-  _ as useItemManagement,
-  F as useRoomManagement,
-  I as useSceneConfiguration,
-  N as useUIState
+  s as ApiService,
+  o as DEFAULT_AVATAR_CONFIG,
+  n as DEFAULT_ROOM_CONFIG,
+  t as DEFAULT_SCENE_CONFIG,
+  i as ManifestDropdown,
+  m as MyRoom,
+  R as MyRoomRepository,
+  M as MyRoomScene,
+  r as VERSION,
+  y as domainConfig,
+  A as getEmbedUrl,
+  g as initializeMyRoomConfig,
+  u as myRoomRepository,
+  C as useAvatarManagement,
+  E as useBackendData,
+  O as useItemManagement,
+  _ as useRoomManagement,
+  F as useSceneConfiguration,
+  I as useUIState
 };
 //# sourceMappingURL=myroom-system.es.js.map
